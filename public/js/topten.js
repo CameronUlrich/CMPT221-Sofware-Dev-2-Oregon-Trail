363055f1a6efbed1915781967517ef6299efbc62
Cameron Ulrich <Cameron.Ulrich1@marist.edu>
2017-10-20 14:32:23 -0400

    moved java script to topten.js

--- public/js/topten.js
+++ public/js/topten.js
@@ -4,6 +4,6 @@
         console.log("spacebar pressed");
     }
   }
-var cars = new Array("Cameron", "Buddy", "Ron", "Kuz", "Mike", "Brendan", "Jacob", "Ben", "Campbell", "Matt");
+var name = new Array("Cameron", "Buddy", "Ron", "Kuz", "Mike", "Brendan", "Jacob", "Ben", "Campbell", "Matt");
           var score = new Array(110, 230, 230, 10, 330, 303, 80, 450, 220, 1110);
           var date = new Array("10/18/2017", "10/18/2017", "10/18/2017", "10/18/2017", "10/18/2017", "10/18/2017", "10/18/2017", "10/18/2017", "10/18/2017", "10/18/2017");
