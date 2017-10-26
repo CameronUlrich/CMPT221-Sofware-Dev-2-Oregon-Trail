1e5dd85dd1b03bc51b250aa384aae2eff08eba11
Cameron Ulrich <Cameron.Ulrich1@marist.edu>
2017-10-26 12:45:22 -0400

    Added sort to score array to go from highest to lowest score.

--- public/js/topten.js
+++ public/js/topten.js
@@ -5,5 +5,6 @@
     }
   }
 var name = new Array("Cameron", "Buddy", "Ron", "Kuz", "Mike", "Brendan", "Jacob", "Ben", "Campbell", "Matt");
-          var score = new Array(11011, 2301, 230, 100, 30, 24, 15, 14, 1, 0);
+          var score = new Array(11, 2301, 230, 100, 30, 24, 15, 14, 1, 0);
+          score.sort(function(a, b){return b - a});
           var date = new Array("10/18/2017", "10/18/2017", "10/18/2017", "10/18/2017", "10/18/2017", "10/18/2017", "10/18/2017", "10/18/2017", "10/18/2017", "10/18/2017");
