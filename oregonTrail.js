34bd8de4600d64f386fda11d50ab95267363e4ad
Cameron Ulrich <Cameron.Ulrich1@marist.edu>
2017-11-02 12:33:50 -0400

    Made gameController and updated oregontrailjs

--- oregonTrail.js
+++ oregonTrail.js
@@ -15,6 +15,18 @@ res.sendFile('views/topten.html', {root: __dirname })
 })
 
 
+// require our gameController (so we can access the data in it)
+var game = require('./controllers/gameController');
+// create an API route so we can request a screen by sending the id (array index)
+app.get('/game/getNewGameScreen/:screenId', function(req, res) {
+// get the screen
+var gameScreen = game.startGameScreens[req.params.screenId];
+//console.log("\n this is the main node file! \n" + gameScreen)
+// return the html
+res.setHeader('Content-Type', 'text/html');
+res.send(gameScreen);
+})
+
 
 
 
